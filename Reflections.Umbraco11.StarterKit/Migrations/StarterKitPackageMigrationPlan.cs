0e835a0fc13dd2c7f81f0bab451415740dafac39
Omar El Sergany <omar@reflections-ibs.com>
2022-11-20 13:10:49 +0200

    Add project files.

---
+++ Reflections.Umbraco11.StarterKit/Migrations/StarterKitPackageMigrationPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace Reflections.Umbraco11.StarterKit.Migrations
+{
+    public class StarterKitPackageMigrationPlan : PackageMigrationPlan
+    {
+        public StarterKitPackageMigrationPlan()
+            : base("Reflections.Umbraco11.StarterKit")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<ImportPackageXmlMigration>(new Guid("CA8673AD-DB69-40DF-BE21-EEA6C9ACB433"));
+        }
+    }
+}
